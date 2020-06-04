00272cf96a17e00f4c35ffe96f795ee409cab087
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-04 16:56:41 +0100

    Delete space

--- spec/feature/feature_bookmarks_spec.rb
+++ spec/feature/feature_bookmarks_spec.rb
@@ -6,6 +6,7 @@ feature 'Bookmark page' do
       Bookmark.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software')
 
     visit("/bookmarks")
+    
     expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
     expect(page).to have_link('Destroy All Software',  href: 'http://www.destroyallsoftware.com')
     expect(page).to have_link('Google', href: 'http://www.google.com')
